60947eaaafa14f2924c052adf27e38d693fce3b2
JaFu.py <104758482+JaFu-py@users.noreply.github.com>
2022-10-24 11:51:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ function expand(objID) {
     }
 }
 
+function question(element, q, a, qNum) {
+    const base = document.querySelector(element);
+    const card = document.createElement("article");
+    const h2 = document.createElement("h2");
+    const input = document.createElement("input");
+
+    card.classList.add("card");
+    h2.textContent = q
+    input.type = "number";
+
+    card.appendChild(h2);
+    card.appendChild(input);
+    base.appendChild(card)
+}
