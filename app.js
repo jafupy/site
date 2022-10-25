0bca0f3f8622326c1080a6bb5eae02aaba1ada0a
JaFu.py <104758482+JaFu-py@users.noreply.github.com>
2022-10-25 14:14:58 +0100

    v2
    Signed-off-by: JaFu.py <104758482+JaFu-py@users.noreply.github.com>

--- app.js
+++
@@ -1,9 +0,0 @@
-function expand(objID) {
-    const obj = document.querySelector("#" + objID);
-    if (obj.classList.contains("expanded")) {
-        obj.classList.remove("expanded")
-    } else {
-        obj.classList.add("expanded")
-    }
-}
-
---
+++ src/app.js
@@ -0,0 +1,6 @@
+/*const container = document.querySelector('.container');
+
+container.addEventListener('wheel', (e)=> {
+    e.preventDefault();
+    container.scrollLeft += e.deltaY;
+}) */
