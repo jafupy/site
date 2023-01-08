c316d57698faeba7d7809213cd513f813d5f34b4
Jacob Furman <jacobf1323@gmail.com>
2023-01-08 08:50:00 +0000

    fancification of name + bg blur fix
    Signed-off-by: Jacob Furman <jacobf1323@gmail.com>

--- scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", function () {
-  let scroll = this.scrollY;
-  body = document.querySelector("body");
-  const clamp = (num, min, max) => (num <= min ? min : num >= max ? max : num);
-  body.style.setProperty("--blur", clamp(scroll / 50, 0, 30) + "px");
-  console.log(clamp(scroll / 20, 0, 30));
-});
